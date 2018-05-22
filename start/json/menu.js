b58487d4eef41c5f227dcbcdd903a2192356988e
Chai <chai@fy.com>
2018-05-22 16:31:42 +0800

    hyadmin

---
+++ start/json/menu.js
@@ -0,0 +1,47 @@
+{
+  "code": 0
+  ,"msg": ""
+  ,"data": [{
+    "title": "主页"
+    ,"icon": "layui-icon-home"
+    ,"list": [{
+      "title": "控制台"
+      ,"jump": "/"
+    }]
+  },  {
+    "name": "pic"
+    ,"title": "轮播图"
+    ,"icon": "layui-icon-auz"
+    ,"list": [{
+        "title": "轮播图",
+        "jump": "pic/swiper"
+    }]
+  },{
+    "name": "set"
+    ,"title": "设置"
+    ,"icon": "layui-icon-set"
+    ,"list": [{
+      "name": "system"
+      ,"title": "系统设置"
+      ,"spread": true
+      ,"list": [{
+        "name": "website"
+        ,"title": "网站设置"
+      },{
+        "name": "email"
+        ,"title": "邮件服务"
+      }]
+    },{
+      "name": "user"
+      ,"title": "我的设置"
+      ,"spread": true
+      ,"list": [{
+        "name": "info"
+        ,"title": "基本资料"
+      },{
+        "name": "password"
+        ,"title": "修改密码"
+      }]
+    }]
+  }]
+}
